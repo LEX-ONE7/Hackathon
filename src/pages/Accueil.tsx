fe5442f8b4575618fe79792e9f0225bd5d2dc25b
Maru23ata97 <maruata.teaurai23@gmail.com>
2022-12-20 09:41:27 -1000

    Blonde Dass

--- src/pages/Accueil.tsx
+++ src/pages/Accueil.tsx
@@ -4,7 +4,12 @@ import Navbar from '../components/Navbar';
 const Accueil = () => {
     return (
       <div>  <Navbar/>
-<h1 className='text-center mt-4'>Accueil</h1>
+<h1 className='text-center mt-4 bg-primary text-white py-5 display-flex'>Accueil </h1>
+<hr/>
+<hr/>
+<h2 className= 'text-center mt-4 py-5 display-flex bg-success'>Presentation</h2>
+<hr/>
+<hr/>
 <Footer/>
 
 </div>
